65935cadefc9c9aa59abea54b7b5a00b90b320f7
Anushka Singh <anushkasingh801@gmail.com>
2026-04-21 14:56:38 +0530

    python context manager, file handling exercises

--- mini-projects/01-basic-etl/main.py
+++ mini-projects/01-basic-etl/main.py
@@ -1,18 +1,21 @@
 # to avoid file not found issue
-import os
-base_path = os.path.dirname(__file__)
-file_path = os.path.join(base_path, "orders.csv")
+# import os
+# base_path = os.path.dirname(__file__)
+# file_path = os.path.join(base_path, "orders.csv")
 
 # ETL code
 import csv
 
 data = []
 
-with open("orders.csv", "r") as file:
-    reader = csv.DictReader(file)
+# CSV = rows of text
+with open("./mini-projects/01-basic-etl/orders.csv", "r") as file:
+    reader = csv.DictReader(file) # DictReader -> turns CSV rows into Python dictionaries using headers as keys
     for row in reader:
         row["amount"] = int(row["amount"])
         data.append(row)
+
+print(data)
 
 # simple transform
 total = sum(row["amount"] for row in data)
